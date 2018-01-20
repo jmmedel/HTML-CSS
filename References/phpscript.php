d3d874749fd37e329431b4aa78bded044872860c
john <jm_sasuke25@yahoo.com>
2018-01-20 13:41:03 +0900

    Add _Some Referense to play around

---
+++ References/phpscript.php
@@ -0,0 +1,10 @@
+
+<?php
+ 
+$usersName = $_GET['name'];
+$usersPassword = $_GET['password'];
+ 
+echo 'Hello ' . $usersName . ' </br>';
+echo 'That is a great password ' . $usersPassword . ' </br>';
+ 
+?>
