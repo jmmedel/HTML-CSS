e2c5c9e5128a88ff4f063a98c4bb16484f779d7e
john <jm.kagaya@gmail.com>
2018-03-14 03:10:24 +0900

    Done
    Done

--- T_004_Comming_soom/js/main.js
+++ T_004_Comming_soom/js/main.js
@@ -1,22 +1,31 @@
 // set The date we`re counting donw to 
-var countdownrate = Date("March 31,2018 00:00:00").getTime();
+var countdownrate =  new Date("March 15,2018 00:00:00").getTime();
+
+
+
 
 // Update the count down every 1second
 var countdownfucntion = setInterval(function(){
-
 // Get todays date and time
-var now = new date().getTime();
+var now = new Date().getTime();
 // Find the distance between now an the count down date
-var distance = countdownrate - now
+var distance = countdownrate - now;
 // Time calculation for days , hours minutes and seconds 
-var days = math.floar
+var days = Math.floor(distance / (1000 * 60 * 60* 24));
+var hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60 ))/ (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 // Output the result in an elemnts with id="demo"
+document.getElementById('demo').innerHTML = days + "d " + hour + "h " + minutes + "m " + seconds + "s ";
 
 // if the count down is over wirte some text
+if(distance < 0 )
+ {
+    clearInterval(countdownfucntion);
+    document.getElementById('demo').innerHTML = "Expired";
 
+ }
 
-
-
-})
+},1000);
 
 
